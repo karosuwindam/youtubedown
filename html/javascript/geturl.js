de5659195769bacea94e099c544db4a9715c14d5
karosuwindam <karosuwindam@gmail.com>
2023-04-05 18:23:25 +0900

    tag edit html add

--- html/javascript/geturl.js
+++ html/javascript/geturl.js
@@ -15,6 +15,38 @@ function downURL(output,url){
     req.send(str);
 }
 
+function pushMp3Tag(jsondata,output,id){
+  var req = new window.XMLHttpRequest();
+  req.onreadystatechange = function(){
+    if(req.readyState == 4 && req.status == 200){
+      var data=req.responseText;
+      data = JSON.parse(data);
+      console.log(data);
+      getMp3Tag(output,id)
+      // document.getElementById(output).innerHTML = data;
+    }
+  };
+  req.open("POST","/edit/"+id,true);
+  req.send(jsondata);
+
+}
+
+function editMp3Tag(output,id) {
+  // var lyrics = document.getElementById("lyrics").innerHTML.replace(/<br>/g, '\r\n')
+  var lyrics = document.getElementById("lyrics").innerText.replace(/\n/g, '\r\n')
+  var json = {
+    "Title":document.getElementById("title").innerHTML,
+    "Artist":document.getElementById("artist").innerHTML,
+    "Album":document.getElementById("album").innerHTML,
+    "Year":document.getElementById("year").innerHTML,
+    "Lyrics":lyrics,
+  }
+  // console.log(json)
+  var jsonstr =JSON.stringify(json)
+  console.log(jsonstr)
+  pushMp3Tag(jsonstr,output,id)
+}
+
 function getMp3List(output) {
 
   var req = new window.XMLHttpRequest();
@@ -30,6 +62,42 @@ function getMp3List(output) {
   req.send( null );
 }
 
+function getMp3Tag(output,id) {
+  var req = new window.XMLHttpRequest();
+  req.onreadystatechange = function(){
+    if(req.readyState == 4 && req.status == 200){
+      var data=req.responseText;
+      data = JSON.parse(data);
+      console.log(data)
+      document.getElementById(output).innerHTML = createMp3Tag(output,data,id);
+    }
+  };
+  req.open("GET","/view/"+id,true);
+  req.send( null );
+}
+
+function createMp3Tag(outputid,data,id){
+  var output = "";
+  output += "<div><button onclick=\""+"editMp3Tag('"+outputid+"'"+",'"+id+"')"+"\">write</button></div>"
+  output += createTextDiv(data.Title,"title")
+  output += createTextDiv(data.Artist,"artist")
+  output += createTextDiv(data.Album,"album")
+  output += createTextDiv(data.Year,"year")
+  output += createTextDiv(data.Lyrics.replace(/\r\n/g, '<br>'),"lyrics")
+  return output
+}
+
+function createTextDiv(str,id) {
+  var output =""
+  if (id=="lyrics"){
+    output += "<div class=\"lyricsrow\">"+id+"</div>"
+  }else{
+    output += "<div class=\"row\">"+id+"</div>"
+  }
+  output += "<div class id=\""+id+"\" contenteditable>"+str+"</div>"
+  return output
+}
+
 function createDownloadLink(data) {
   var output = ""
   for(var i=0;i<data.length;i++) {
@@ -37,7 +105,8 @@ function createDownloadLink(data) {
     if (i>0) {
       output += "<br>"
     }
-    output += "<a href='/mp3/"+tmp.No+"'>"+tmp.Name+"<a>"
+    output += "<a href='/mp3/"+tmp.No+"'>"+tmp.Name+"<a>"+" "+"<audio controls src=\"/mp3/"+tmp.No+"\" type=\"audio/mp3\">"+"</audio>"
+    output += "<button onclick=\""+"getMp3Tag('edit','"+tmp.No+"')"+"\">view</button>"
   }
   return output
 }
